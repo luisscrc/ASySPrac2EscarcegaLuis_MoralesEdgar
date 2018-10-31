c76cc4d80c497e0c76d2bf8b0468a6b5856b5c26
luisscrc <32582552+luisscrc@users.noreply.github.com>
2018-10-31 15:36:22 -0600

    j

---
+++ ASySPrac2LuisEscarcega.m
@@ -0,0 +1,19 @@
+%% Práctica 2: Señales continuas 
+% Análisis de Señales y Sistemas 
+    % Escárcega Corona Luis 
+    % Morales Sanabria Edgar Esteban
+        % 2MV1
+        
+ 
+%% Objetivo:
+
+
+%% Crear función fun1 
+%% Crear función que grafique valores de $f:R\rightarrow R$
+%% Crear función que grafique valores de $f:R\rightarrow R^n$
+%% Lathi secciones M1.1 a M1.4
+%% Aplicando el software
+    % El siguiente problema se encuentra en el libro Signals de Lathi y es el 1.2.2 
+    %
+%% Referencias
+ 
---
+++ fun1.m
@@ -0,0 +1,9 @@
+function fun1(w,a)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+a=input('Ingrese el valor de a ');
+w=input('Ingrese el valor de w ');
+fun1 = a/(a^2+w^2)
+
+end
+
